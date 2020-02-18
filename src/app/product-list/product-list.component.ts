95d4cb0de049445ad1cc7f3f1338767691c9e914
APP技術部-李慧茹 <lala.lee@mitake.com.tw>
2020-02-18 19:41:53 +0800

    增加 products 陣列 到 app-product-list 元件

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  products = [
+    {
+      name: 'Phone XL',
+      price: 799,
+      description: 'A large phone with one of the best screens'
+    },
+    {
+      name: 'Phone Mini',
+      price: 699,
+      description: 'A great phone with one of the best cameras'
+    },
+    {
+      name: 'Phone Standard',
+      price: 299,
+      description: ''
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
