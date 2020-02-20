dd4aeeb090976d9742db46bc5ec29cd95754bd7a
APP技術部-李慧茹 <lala.lee@mitake.com.tw>
2020-02-20 15:20:10 +0800

    在元件中使用 Service 的資料

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertComponent } from './product-alert/product-alert.component';
 import { SharedModule } from '../shared/shared.module';
