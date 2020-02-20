873bcc6146a1ed706fb57ea873eaae5ad03f922f
APP技術部-李慧茹 <lala.lee@mitake.com.tw>
2020-02-21 00:14:30 +0800

    調整 app-routing.module.ts 把 CartMdoule 做 Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductListComponent } from './product/product-list/product-list.component';
 import { ProductDetailComponent } from './product/product-detail/product-detail.component';
-import { CartComponent } from './cart/cart/cart.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'product', pathMatch: 'full' },
   { path: 'product', component: ProductListComponent },
   { path: 'product/:id', component: ProductDetailComponent },
-  { path: 'cart', component: CartComponent}
+  {
+    path: 'cart',
+    loadChildren: () => import('./cart/cart.module').then(m => m.CartModule)
+  }
 ];
 
 @NgModule({
