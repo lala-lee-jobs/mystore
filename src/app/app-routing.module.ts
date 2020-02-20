37d034148ce90d4aff06859ea28df12728620f87
APP技術部-李慧茹 <lala.lee@mitake.com.tw>
2020-02-20 23:50:07 +0800

    在 app-routing.module.ts 加上 cart 的路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductListComponent } from './product/product-list/product-list.component';
 import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { CartComponent } from './cart/cart/cart.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'product', pathMatch: 'full' },
   { path: 'product', component: ProductListComponent },
-  { path: 'product/:id', component: ProductDetailComponent }
+  { path: 'product/:id', component: ProductDetailComponent },
+  { path: 'cart', component: CartComponent}
 ];
 
 @NgModule({
