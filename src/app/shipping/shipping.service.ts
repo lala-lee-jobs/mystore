8d329a3c440f516e1fe8ca43aebd72556349005e
APP技術部-李慧茹 <lala.lee@mitake.com.tw>
2020-02-21 11:50:04 +0800

    在 Shipping Service 使用 JSON Server 的資料

--- src/app/shipping/shipping.service.ts
+++ src/app/shipping/shipping.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShippingService {
 
-  constructor() { }
+  private url = `${environment.apiurl}\shipping`;
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  getItems() {
+    return this.httpClient.get(this.url);
+  }
 }
