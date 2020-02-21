7eb70145597150acb403730f73071c371bd183bb
APP技術部-李慧茹 <lala.lee@mitake.com.tw>
2020-02-21 11:53:54 +0800

    在Shipping元件使用Shipping服務取得API資料

--- src/app/shipping/shipping/shipping.component.ts
+++ src/app/shipping/shipping/shipping.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ShippingService } from '../shipping.service';
 
 @Component({
   selector: 'app-shipping',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShippingComponent implements OnInit {
 
-  constructor() { }
+  shippingCosts;
+
+  constructor(
+    private shippingService: ShippingService
+  ) { }
 
   ngOnInit(): void {
+    this.shippingCosts = this.shippingService.getItems();
   }
 
 }
