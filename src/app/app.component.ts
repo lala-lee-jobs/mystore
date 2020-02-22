95001328a90359a782b926e74ab9bf64f457808f
APP技術部-李慧茹 <lala.lee@mitake.com.tw>
2020-02-22 11:54:15 +0800

    filter 過濾出符合條件值的資料流 - 在根元件監聽 RouterEvent 的變化，只印出 NavigationEnd Event 的值

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mystore';
+
+  constructor(
+    private router: Router
+  ) {
+    // this.router.events.subscribe(event => {
+    //   console.log(event);
+    // });
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(event => {
+      console.log(event);
+    });
+  }
 }
